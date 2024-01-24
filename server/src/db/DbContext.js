e180ec7b3cc70fe2622dbd958a04b4aad61e8e39
DaDrwin <darienjensen@outlook.com>
2024-01-24 15:31:53 -0700

    trade stuff

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { TradeSchema } from '../models/Trade.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Trade = mongoose.model('Trade', TradeSchema);
 }
 
 export const dbContext = new DbContext()
