c2ed51798cd840e8359438459ac040baf4e3340c
DaDrwin <darienjensen@outlook.com>
2024-01-26 15:54:17 -0700

    heh

--- server/src/controllers/TradeController.js
+++ server/src/controllers/TradeController.js
@@ -59,7 +59,7 @@ export class TradeController extends BaseController {
             next(error)
         }
     }
-    async getTrade(req, res, next) {
+    async getTradeById(req, res, next) {
         try {
             res.send(await tradeService.getTradeById(req.params.id))
         }
