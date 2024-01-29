ec2f945ef1cbee43f08301510191f3c559beec99
Brooke Lacey <brooke@brookejlacey.com>
2024-01-29 15:48:52 -0700

    changed MinLength

--- server/src/models/Project.js
+++ server/src/models/Project.js
@@ -5,9 +5,9 @@ export const ProjectSchema = new Schema({
   type: {},
   archived: { type: Boolean, required: true, default: false },
   coverImg: { type: String, required: true, maxlength: 500 },
-  description: { type: String, required: true, minlength: 20, maxlength: 1000 },
+  description: { type: String, required: true, minlength: 5, maxlength: 1000 },
   creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
-  tradeId: {type: Schema.Types.ObjectId, required: true, ref: 'Trade'}
+  tradeId: { type: Schema.Types.ObjectId, required: true, ref: 'Trade' }
 },
   {
     timestamps: true,
