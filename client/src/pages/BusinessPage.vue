351fd82bb2e73f4e614670a70dbc0e820b763027
Heather Floyd <heatherflo@gmail.com>
2024-01-30 21:11:45 -0700

    more headers in place

--- client/src/pages/BusinessPage.vue
+++ client/src/pages/BusinessPage.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="BusinessPage">
+  <div class="BusinessPage bg">
+    <section class="row">
+      <div class="col-7 d-flex justify-content-start">
+        <RouterLink :to="{ name: 'Home' }">
+          <img class=" hammer mb-2 img-fluid" src="../assets/img/hammer.png" alt="cartoon hammer">
+        </RouterLink>
+      </div>
+      <div class="col-5 d-flex justify-content-end">
+
+        <Login />
+      </div>
+    </section>
     this is the business page
 
   </div>
@@ -17,4 +28,14 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.hammer {
+  height: 100px;
+  width: 100px
+}
+
+.bg {
+  height: 100vh;
+  background-color: #F4FDFF;
+}
+</style>
