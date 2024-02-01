f807b0e682bb206adac96f7c590ea98e87f7d155
Heather Floyd <heatherflo@gmail.com>
2024-01-31 21:27:23 -0700

    committing for fun

--- client/src/components/ProfileCard.vue
+++ client/src/components/ProfileCard.vue
@@ -2,12 +2,12 @@
     <router-link to="">
 
         <span class="d-flex align-items-center rounded-pill border border-dark">
-            
-            <img :src="profile.picture" alt="" class="profile-picture border border-start-0 border-dark ">
-            
-            <span class="m-2 text-dark">{{profile.name}}</span>
-            
-            
+
+            <img :src="profile.picture" alt="" class="profile-picture border  border-dark ">
+
+            <span class="m-2 text-dark">{{ profile.name }}</span>
+
+
         </span>
 
     </router-link>
@@ -19,25 +19,23 @@ import { AppState } from '../AppState';
 import { computed, ref, onMounted } from 'vue';
 import { Account } from '../models/Account';
 export default {
-  props: { profile: { type: Account, required: true } },
+    props: { profile: { type: Account, required: true } },
 
-    setup(){
-    return { }
+    setup() {
+        return {}
     }
 };
 </script>
 
 
 <style lang="scss" scoped>
+.profile-picture {
+    border-radius: 50%;
+    height: 7vh;
+    width: 7vh;
+}
 
-    .profile-picture{
-        border-radius: 50%;
-        height: 7vh;
-        width: 7vh;
-    }
-
-    span{
-        width: fit-content;
-    }
-
+span {
+    width: fit-content;
+}
 </style>
