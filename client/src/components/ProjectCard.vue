56b3475bad3cdd3b1ccfaf09c70a0cbc09c71d36
Heather Floyd <heatherflo@gmail.com>
2024-02-02 15:15:13 -0700

    projectcard done

--- client/src/components/ProjectCard.vue
+++ client/src/components/ProjectCard.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    <img class="profile-picture p-2" :src="project.creator.picture" alt="">
+  <div class=" project-card  mb-2 selectable ">
+
+    <img class="profile-picture p-2 image-fluid" :src="project.creator.picture" alt="">
+
     {{ project.creator.name }}
 
-    <div class="text-center fs-4">
+    <div class=" fs-4 ms-2 mb-2">
       {{ project.title }}
     </div>
-    <img class="img" :src="project.coverImg" :alt="project.coverImg">
+    <img class="image-fluid img" :src="project.coverImg" :alt="project.coverImg">
 
   </div>
 </template>
@@ -37,13 +39,21 @@ export default {
 <style lang="scss" scoped>
 .profile-picture {
   border-radius: 50%;
-  height: 7vh;
-  width: 7vh;
+  max-height: 75px;
+  max-width: 75px;
+}
+
+.project-card {
+  border-radius: 15px;
+  overflow: hidden;
+  border: solid .1em;
+  border-color: #214E34;
+
 }
 
 .img {
-  overflow: hidden;
-  height: 23vh;
+  min-width: 100%;
+  max-height: 30vh;
   object-fit: cover;
   object-position: center;
 }
